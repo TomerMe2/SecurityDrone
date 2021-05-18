a7e9ff3e736f525a350a98eda76b79debfa8c1db
shaharewq0 <cohenhas@post.bgu.ac.il>
2021-05-18 12:48:49 +0300

    updated info page due to pixel overflow error.

--- security_drone_user_app/lib/presentation/pages/info_page.dart
+++ security_drone_user_app/lib/presentation/pages/info_page.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:security_drone_user_app/presentation/text_section.dart';
 
-class InfoPage extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return InfoPageState();
-  }
-}
+class InfoPage extends StatelessWidget {
 
-class InfoPageState extends State<InfoPage> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -16,18 +10,19 @@ class InfoPageState extends State<InfoPage> {
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          TextSection("Project vision:", "Provide security using a drone and AI.\n"
-              + "Provide more time for our user to react to intrusions.\n"
-              + "More specifically, give the user enough information about an intrusion to react quickly,\n"
+          TextSection("Project vision:", "Provide security using a drone and AI\n"
+              + "More specifically, give the user enough information about an intrusion to react quickly, "
               + "and the ability to track the thief and have liable documentation of the crime.",
-            height: 150.0,
+            height: 160.0,
           ),
-          TextSection("Developer Team:", "Daniel Katz\nTomer Laor\nShahar Cohen Hadad"),
+          TextSection("Developer Team:", "Daniel Katz\nTomer Laor\nShahar Cohen Hadad",
+            height: 90.0,
+          ),
         ],
       ),
       constraints: BoxConstraints(
-        maxHeight: 100.0,
-        maxWidth: 100.0
+          maxHeight: 100.0,
+          maxWidth: 100.0
       ),
     );
   }
